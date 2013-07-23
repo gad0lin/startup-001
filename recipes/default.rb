65a04acbb00857d8bd2707e5ec5144692d61f75b
Krzysztof Barczynski <krzysztof.barczynski@roclets.net>
2013-07-23 00:04:57 -0700

    Run heroku + setup.sh

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,19 +14,38 @@ package "git-core"
 
 directory "/opt/startup"
 
-script "install_heroku" do 
-    action :nothing
-    code <<-EOH
-     /opt/startup/heroku-install.sh
-    EOH
+bash "install_heroku" do 
+  action :nothing
+  cwd "/opt/startup"
+  code <<-EOH
+    sudo chmod +x /opt/startup/heroku-install.sh
+    sudo /opt/startup/heroku-install.sh
+  EOH
+  user "root"
+  group "root"
 end
-
 
 remote_file "/opt/startup/heroku-install.sh" do
-   source "https://toolbelt.heroku.com/install-ubuntu.sh"
-   not_if { ::File.exists?("/opt/startup/heroku-install.sh") } 
-   notifies :run, "script[install_heroku]", :immediately
+  source "https://toolbelt.heroku.com/install-ubuntu.sh"
+  mode 0755
+  not_if { ::File.exists?("/opt/startup/heroku-install.sh") } 
+  notifies :run, "bash[install_heroku]", :immediately
 end
 
+bash "setup" do 
+  action :nothing
+  cwd "/opt/startup-setup"
+  code <<-EOH
+    sudo chmod +x /opt/startup-setup/setup.sh
+    sudo /opt/startup-setup/setup.sh
+  EOH
+end
 
-
+git "/opt/startup-setup" do
+  repository "https://github.com/startup-class/setup.git"
+  reference "master"
+  action :sync
+  user "root"
+  group "root"
+  notifies :run, "bash[setup]", :immediately
+end
